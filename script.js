ea308591f08711380489c0d5b5a44228c721d2fa
Corentin Deblock <corentindeblock@gmail.com>
2019-05-21 11:26:30 +0200

    Finition de l'exercice 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let languages = "fr";
+let languages = prompt("Choissisez votre langue");
 let message = "";
 
 if(languages == "fr"){
@@ -11,3 +11,30 @@ if(languages == "fr"){
     message = "Langue non défini";
 }
 console.log(message);
+
+let score = 80;
+let result;
+
+if(score >= 90){
+    result = "A";
+}else if(score < 90){
+    result = "B"
+}else if(score <= 50){
+    result = "C"
+}else{
+    result = "D"
+}
+
+console.log(`Votre note est : ${result}`);
+
+let sing = "orange";
+let numberFruits = 3;
+let resultFruits;
+
+if(numberFruits > 1){
+    sing += "s";
+}
+
+resultFruits = `J'ai ${numberFruits} ${sing}`;
+
+console.log(resultFruits);
