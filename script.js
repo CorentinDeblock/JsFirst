c19c41dfe6579649d2e99152b6e295d4707a1d50
Corentin Deblock <corentindeblock@gmail.com>
2019-05-22 10:29:03 +0200

    Finition de l'exercice 8

--- script.js
+++ script.js
@@ -1,83 +1,34 @@
 let character = {
-    name:"Jean",
-    age:40,
-    item_to_give: [],
-}
-function giveItem(perso){
-    let index = Math.random() * perso.item_to_give.length;
-    let save = perso.item_to_give[index];
-    perso.item_to_give[index] = "";
-    return save;
-}
-
-for(property in character){
-    console.log(character[property]);
-}
-
-let sword = {
-    title: "Sword",
-    physics: 10,
-    magic: 0,
-    minLevel: 1,
-    avaible: true,
-}
-let hache = {
-    title: "Axe",
-    physics: 20,
-    magic: 0,
-    minLevel: 4,
-    avaible: true,
-}
-let sceptre = {
-    title: "Scepter",
-    physics: 2,
-    magic: 40,
-    minLevel: 6,
-    avaible: true,
-}
-let spear = {
-    title: "Spear",
-    physics: 15,
-    magic: 0,
-    minLevel: 1,
-    avaible: true,
-}
-
-let shop = {
-    sell: [sword,hache,sceptre,spear],
-    show: function() {
-        for(let i = 0;i < sell.length;i++){
-            console.log(sell[i].name);
-        }
-        console.log(this.sell.length);
-    },
-    showAvaible: function(){
-        for(let i = 0; i < this.sell.length;i++){
-            if(this.sell[i].avaible){
-                console.log(this.sell[i].name);
-            }
-        }
-    },
-    showLowLevel: function(){
-        for(let i = 0; i < this.sell.length;i++){
-            if(this.sell[i].level <= 10){
-                console.log(this.sell[i].name);
-            }
-        }
-    }
-}   
-
-shop.show();
-
-let mainCharacter = {
-    title: "",
-    level: 10,
-    life: 100,
+    name: "",
+    level: 0,
+    life: 0,
     weapon: {
         name: "",
-        damage: 20,
+        damage: 0,
     },
-    attack:function(){
-        console.log(`${name} attaque avec l'arme ${weapon.name} les degâts sont ${weapon.damage * level}`)
+    attack: function(target){
+        console.log(`${this.name} attaque ${target.name}`);
+        console.log(`avec l'arme ${this.weapon.name}`);
+        console.log(`et lui inflige ${this.weapon.damage * this.level} de dégats`);
+        target.receiveDamage(this.weapon.damage * this.level);
+        console.log(`${target.name} a maintenant ${target.life} de vie`);
+    },
+    receiveDamage: function(damage){
+        this.life -= damage;
     },
 }
+let opponentCharacter = Object.create(character);
+opponentCharacter.weapon.name = "Sword";
+opponentCharacter.weapon.damage = 10;
+opponentCharacter.life = 100;
+opponentCharacter.level = 2;
+opponentCharacter.name = "michel";
+
+let mainCharacter = Object.create(character);
+mainCharacter.weapon.name = "Sceptre";
+mainCharacter.weapon.damage = 20;
+mainCharacter.level = 4;
+mainCharacter.name = "Jean";
+mainCharacter.life = 100;
+
+mainCharacter.attack(opponentCharacter);
