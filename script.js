35daeb205704287bafee0a8f9f08580bfccde8e9
Corentin Deblock <corentindeblock@gmail.com>
2019-05-22 11:26:22 +0200

    Push de l'exercice

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
-let character = {
-    name: "",
-    level: 0,
-    life: 0,
-    weapon: {
-        name: "",
-        damage: 0,
-    },
-    attack: function(target){
-        console.log(`${this.name} attaque ${target.name}`);
-        console.log(`avec l'arme ${this.weapon.name}`);
-        console.log(`et lui inflige ${this.weapon.damage * this.level} de dégats`);
-        target.receiveDamage(this.weapon.damage * this.level);
-        console.log(`${target.name} a maintenant ${target.life} de vie`);
-    },
-    receiveDamage: function(damage){
-        this.life -= damage;
-    },
+let message = "";
+let essai = 0;
+
+let min = 20;
+let max = 80;
+
+function randomNum(){
+    return Math.ceil(Math.random() * (max - min) + min); 
 }
-let opponentCharacter = Object.create(character);
-opponentCharacter.weapon.name = "Sword";
-opponentCharacter.weapon.damage = 10;
-opponentCharacter.life = 100;
-opponentCharacter.level = 2;
-opponentCharacter.name = "michel";
+let number = randomNum();
 
-let mainCharacter = Object.create(character);
-mainCharacter.weapon.name = "Sceptre";
-mainCharacter.weapon.damage = 20;
-mainCharacter.level = 4;
-mainCharacter.name = "Jean";
-mainCharacter.life = 100;
-
-mainCharacter.attack(opponentCharacter);
+function asker(val){
+    if(val == number){
+        return `C'est juste tu a trouver en ${essai} coups`;
+    }else if(val < number){
+        return "C'est plus";
+    }
+    return "C'est moins";
+}
+while(true){
+    let ask = prompt("Entrer une valeur entre 20 et 80");
+    alert(asker(ask));
+    if(ask == number){
+        break;
+    }
+    essai++;
+}
