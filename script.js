412001ba249e4adb032d4f07d935ffda75dfefab
Corentin Deblock <corentindeblock@gmail.com>
2019-05-23 16:31:44 +0200

    Finition de l'exercice

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
-let hover = document.querySelectorAll(".hoverMe")
+let charac = document.querySelector("#character");
 
-hover.forEach((elem) => {
-    elem.addEventListener("mouseenter",function(){
-        this.style.opacity = 0;
-    })
-})
-
-let reset = document.querySelector("#reset");
-
-reset.addEventListener("click",function(){
-    hover.forEach((elem) => {
-        elem.style.opacity = 1;
-    })
-})
-
-let x = document.querySelector("#axe-x");
-let y = document.querySelector("#axe-y");
-
-document.onmousemove = () => {
-    x.textContent = event.x;
-    y.textContent = event.y;
+function changeBg(color){
+    charac.style.backgroundColor = color;
 }
+
+function bgChange(event){
+    switch(event.key){
+        case "0":
+            changeBg("red");
+            break;
+        case "1":
+            changeBg("blue");
+            break;
+        case "2":
+            changeBg("orange");
+            break;
+        case "3":
+            changeBg("green");
+            break;
+        case "4":
+            changeBg("black");
+            break;
+        case "5":
+            changeBg("purple");
+            break;
+        case "6":
+            changeBg("pink");
+            break;
+        case "7":
+            changeBg("yellow");
+            break;
+        case "8":
+            changeBg("brown");
+            break;
+        case "9":
+            changeBg("grey");
+            break;
+        default:
+            
+    }
+}
+
+document.addEventListener("keydown",bgChange);
